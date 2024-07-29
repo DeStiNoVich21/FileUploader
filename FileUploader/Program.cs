b2e8d18d301fb628760ce2f42876f755f0a36a48
Dimas <Dimas@ICEY_LAPTOP>
2024-07-29 10:16:25 +0500

    dockerfile fix

--- FileUploader/Program.cs
+++ FileUploader/Program.cs
@@ -8,19 +8,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Policy", corsBuilder =>
+    {
+        corsBuilder.WithOrigins(builder.Configuration["CorsUrls"].Split(";"))
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 builder.Services.AddScoped<IServices, Services>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+app.UseCors("Policy");
 
 app.UseAuthorization();
 
